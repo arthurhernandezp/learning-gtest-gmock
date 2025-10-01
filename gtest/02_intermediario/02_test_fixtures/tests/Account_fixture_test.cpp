db9ba5497e1c1b4b90360eb7bb8d854c1b71c98b
Arthur Hernandez <arthurhernandezp@gmail.com>
2025-10-01 09:15:39 -0300

    Cenário de teste com classes com fixture tests

---
+++ gtest/02_intermediario/02_test_fixtures/tests/Account_fixture_test.cpp
@@ -0,0 +1,61 @@
+#include "Account.hpp"
+#include <gtest/gtest.h>
+
+class AccountTestFixture : public testing::Test
+{
+    public:
+        void SetUp() override;
+        void TearDown() override;
+    protected:
+        Account account;
+};
+
+//Agora todos os testes que usarem esse test fixture começaram com uma account com 10.5 no balance
+void AccountTestFixture::SetUp()
+{
+    std::cout << "SetUp Called" << '\n';
+    account.deposit(10.5);
+}
+
+TEST_F(AccountTestFixture, TestEmptyAccount)
+{
+    ASSERT_EQ(10.5,account.getBalance());
+}
+
+TEST_F(AccountTestFixture,TestDeposit)
+{
+    account.deposit(100);
+    ASSERT_EQ(110.5,account.getBalance());
+}
+
+TEST_F(AccountTestFixture,TestWithdrawOK)
+{
+    account.withdraw(5);
+    ASSERT_EQ(5.5,account.getBalance());
+}
+
+TEST_F(AccountTestFixture,TestWithdrawInsufficientFunds)
+{
+    ASSERT_THROW(account.withdraw(300);,std::runtime_error);
+}
+
+TEST_F(AccountTestFixture,TestTransferTo)
+{
+    //Arrange
+    Account dest;
+
+    //Act
+    account.transfer(dest,2);
+
+    //Assert
+    ASSERT_EQ(8.5,account.getBalance());
+    ASSERT_EQ(2,dest.getBalance());
+}
+
+TEST_F(AccountTestFixture,TestTransferInsufficientFundsTo)
+{
+    Account dest;
+    ASSERT_THROW(account.transfer(dest,200),std::runtime_error);
+}
+
+
