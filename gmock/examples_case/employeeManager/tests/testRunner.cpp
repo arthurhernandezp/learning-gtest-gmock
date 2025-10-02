22a1a9522e2a3e01d28eff0059e91b5d7c884d36
Arthur Hernandez <arthurhernandezp@gmail.com>
2025-10-02 13:58:15 -0300

    Definindo comportamentos de funções mockadas usando ACTION,exception, freefunction/lambdafunction e member function

--- gmock/examples_case/employeeManager/tests/testRunner.cpp
+++ gmock/examples_case/employeeManager/tests/testRunner.cpp
@@ -1,4 +1,6 @@
 #include <map>
+#include <stdexcept>
+#include <iostream>
 
 #include "IDatabaseConnection.hpp"
 #include "Employee.hpp"
@@ -25,6 +27,12 @@ class MockDatabaseConnection : public IDatabaseConnection
         MOCK_METHOD(void,updateSalary,(int,float));
         MOCK_METHOD((std::vector<Employee>),getSalariesRange,(float),(const));
         MOCK_METHOD((std::vector<Employee>),getSalariesRange,(float,float),(const));
+
+        void someMemberErrorFunction()
+        {
+            std::cout << "Member method called" << '\n';
+            throw std::runtime_error("Dummy error");
+        }
 };
 
 MockDatabaseConnection::MockDatabaseConnection(std::string serverAddress) : IDatabaseConnection(serverAddress)
@@ -75,4 +83,72 @@ TEST(TestEmployeeManager, TestGetSalary)
     float employeeSalary = employeeManager.getSalary(employeeId);
 
     ASSERT_EQ(salary,employeeSalary);
+}
+
+// Formas de dizer ao google mock o que irá acontecer ao chamar uma função mockada especifica 
+
+//Usando exception
+TEST(TestEmployeeManager, TestConnectionError)
+{
+    MockDatabaseConnection dbConnection("dummyConnection");
+
+    EXPECT_CALL(dbConnection,connect()).WillOnce(testing::Throw(std::runtime_error("Dummy error")));
+
+    ASSERT_THROW(EmployeeManager employeeManager(&dbConnection),std::runtime_error);
+}
+
+ACTION(myThrow)
+{
+    std::cout << "Throwing an error" << '\n';
+    throw std::runtime_error("Dummy error");
+}
+
+//Usando ACTION
+TEST(TestEmployeeManager, TestConnectionErrorAction)
+{
+    MockDatabaseConnection dbConnection("dummyConnection");
+    
+    EXPECT_CALL(dbConnection,connect()).WillOnce(myThrow());
+
+    ASSERT_THROW(EmployeeManager employeeManager(&dbConnection),std::runtime_error);
+}
+
+void someFreeFunction()
+{
+    std::cout << "someFreeFunction" << '\n';
+    throw std::runtime_error("Dummy error");
+}
+
+//Usando freefunction
+TEST(TestEmployeeManager, TestConnectionErrorInvoke)
+{
+    MockDatabaseConnection dbConnection("dummyConnection");
+    
+    EXPECT_CALL(dbConnection,connect()).WillOnce(testing::Invoke(someFreeFunction));
+
+    ASSERT_THROW(EmployeeManager employeeManager(&dbConnection),std::runtime_error);
+}
+
+// Usando lambda function
+TEST(TestEmployeeManager, TestConnectionErrorLambdaInvoke)
+{
+    MockDatabaseConnection dbConnection("dummyConnection");
+    
+    EXPECT_CALL(dbConnection,connect()).WillOnce(testing::Invoke([]()
+    {
+        std::cout << "lambda function" << '\n';
+        throw std::runtime_error("Dummy error");
+    }));
+
+    ASSERT_THROW(EmployeeManager employeeManager(&dbConnection),std::runtime_error);
+}
+
+// Usando member function
+TEST(TestEmployeeManager, TestConnectionErrorWithMemberFunctionInvoke)
+{
+    MockDatabaseConnection dbConnection("dummyConnection");
+    auto boundMethod = std::bind(&MockDatabaseConnection::someMemberErrorFunction,&dbConnection);
+    EXPECT_CALL(dbConnection,connect()).WillOnce(testing::InvokeWithoutArgs(boundMethod));
+
+    ASSERT_THROW(EmployeeManager employeeManager(&dbConnection),std::runtime_error);
 }
